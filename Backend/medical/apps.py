58e7ab7a9d1e8805a255cac71e55f763e54eae0d
Jabrahamjohn <Jabrahamjohns@gmail.com>
2025-10-21 17:22:02 +0300

    feat: Update medical app configuration to use custom AppConfig and adjust settings

--- Backend/medical/apps.py
+++ Backend/medical/apps.py
@@ -1,6 +1,10 @@
+# medical/apps.py
 from django.apps import AppConfig
-
 
 class MedicalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'medical'
+
+    def ready(self):
+        # import signal handlers
+        from . import signals  # noqa
--- Backend/sgss_medical_fund/settings.py
+++ Backend/sgss_medical_fund/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'medical'
+    'medical.apps.MedicalConfig',
 ]
 
 MIDDLEWARE = [
